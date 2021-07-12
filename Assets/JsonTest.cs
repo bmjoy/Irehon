ad97827d816240f76983af6c9bf5a8f0fd0d5ac1
re-mouse <fmousegames@gmail.com>
2021-07-12 14:09:03 +0300

    Started work with json

---
+++ Assets/JsonTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonTest : MonoBehaviour
+{
+    private void Start()
+    {
+        string json = Resources.Load<TextAsset>("Items").ToString();
+        print(json);
+    }
+}
