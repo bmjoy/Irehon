ab63e584f2aa006ca2a97c740681ff70ff10d04d
Harren Leilani <hleilani@va-j5.kzn.21-school.ru>
2021-07-17 15:06:48 +0300

    Inventory pre support

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject inventorySlotPrefab;
+    [SerializeField]
+    private RectTransform inventory;
+    [SerializeField]
+    private RectTransform containerWindow;
+    public static InventoryManager instance;
+
+    private void Awake()
+    {
+        if (instance != this && instance != null)
+            Destroy(this);
+        else
+            instance = this;
+    }
+
+    public void OpenInventory(Container container)
+    {
+        FillContainer(inventory, container);
+    }
+
+    private void FillContainer(RectTransform containerBG, Container container)
+    {
+        foreach (ContainerSlot containerSlot in container.slots)
+        {
+
+        }
+    }
+}
