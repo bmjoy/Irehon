a51dccd1c6d405254ab3b49c6cd30e608bbacba3
Harren Leilani <hleilani@ev-s1.kzn.21-school.ru>
2021-08-06 11:44:49 +0300

    UI

---
+++ Assets/TooltipWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+
+public class TooltipWindow : MonoBehaviour
+{
+    public static TooltipWindow i;
+
+    private void Awake()
+    {
+        if (i != null && i != this)
+            Destroy(gameObject);
+        else
+            i = this;
+    }
+
+    public static ShowTooltip()
+}
