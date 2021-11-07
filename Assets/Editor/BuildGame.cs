1e774be3233143649cbf97aaef9ecfee767a618e
re-mouse <fmousegames@gmail.com>
2021-11-07 16:38:57 +0300

    Added build script

---
+++ Assets/Editor/BuildGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+public class BuildGame
+{
+    public static void BuildGameServers()
+    {
+        BuildServerNorth();
+        BuildServerCenter();
+    }
+
+    public static void BuildServerNorth()
+    {
+        string[] scenes = { "Assets/Scenes/North.unity" };
+        BuildPipeline.BuildPlayer(scenes, "builds/North/North.exe", BuildTarget.StandaloneLinux64, BuildOptions.EnableHeadlessMode);
+    }
+
+    public static void BuildServerCenter()
+    {
+        string[] scenes = { "Assets/Scenes/Center.unity" };
+        BuildPipeline.BuildPlayer(scenes, "builds/Center/Center.exe", BuildTarget.StandaloneLinux64, BuildOptions.EnableHeadlessMode);
+    }
+}
