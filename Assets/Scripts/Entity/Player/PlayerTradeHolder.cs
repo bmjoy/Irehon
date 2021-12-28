b0c1459c55910f965ebb776018696d5f9e9ed996
Nyiaz <64260094+re-mouse@users.noreply.github.com>
2021-12-28 12:37:19 +0300

    Added graphic settings

---
+++ Assets/Scripts/Entity/Player/PlayerTradeHolder.cs
@@ -0,0 +1,20 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Irehon
+{
+    [RequireComponent(typeof(Player))]
+    public class PlayerTradeHolder : NetworkBehaviour
+    {
+        private Player player;
+
+        private void Start()
+        {
+            player = GetComponent<Player>();
+        }
+
+
+    }
+}
