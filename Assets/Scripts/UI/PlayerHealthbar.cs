8ae63921091643b77a50aaaf52169435c29dc9b6
Nyiaz <64260094+re-mouse@users.noreply.github.com>
2022-02-09 18:50:19 +0300

    Fix craft button bug, add block animation

--- Assets/Scripts/UI/PlayerHealthbar.cs
+++ Assets/Scripts/UI/PlayerHealthbar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Irehon.UI
 {
@@ -10,6 +11,6 @@ namespace Irehon.UI
     {
         public static PlayerHealthBar Instance;
 
-        private void Awake() => Instance = this;
+        private void Awake() => Instance = this; 
     }
 }
