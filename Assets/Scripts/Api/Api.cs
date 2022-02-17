ee16b350f448bee16188c3d3b929a5b64f510f42
Nyiaz <64260094+re-mouse@users.noreply.github.com>
2022-02-17 18:16:26 +0300

    Updated api key

--- Assets/Scripts/Api/Api.cs
+++ Assets/Scripts/Api/Api.cs
@@ -8,7 +8,7 @@ namespace Irehon.CloudAPI
     public enum ApiMethod { GET, POST, PUT, DELETE };
     public static class Api
     {
-        private const string API_KEY_COOKIE = "AUTH=#i#Li`f2D[W?{$pSL`@4=gvy?1[?gLOyMOUc*TpPdZYKZhj`#%@D^CQ<aR@XwJM";
+        private const string API_KEY_COOKIE = "AUTH=#i#Li`f2D[W?{$pSL`@4=gvy?1[?gLOyMOUc*TpPdIPHBUJKPUIPYFG&FUGI(FHFG&*FGUIF&IGFYU&FIGUYILFUZYKZhj`#%@D^CQ<aR@XwJM";
 
 
         public static UnityWebRequest Request(string request, ApiMethod method = ApiMethod.GET)
