4a856dfd8e2568ef54d813ae6b049816fd2e2171
Lex <totallabyss@gmail.com>
2022-02-25 17:45:01 +0300

    fix bow

--- Assets/Scripts/Entity/Player/PlayerStates/PlayerWalkState.cs
+++ Assets/Scripts/Entity/Player/PlayerStates/PlayerWalkState.cs
@@ -33,7 +33,7 @@ public class PlayerWalkState : PlayerRotatableState
     {
         base.HandleInput(input, isServer);
 
-        if (input.IsKeyPressed(KeyCode.Mouse1))
+        if (input.IsKeyPressed(KeyCode.Mouse1) && this.abilitySystem.playerWeaponEquipment.GetWeapon().GetType() != WeaponType.Bow)
         {
             return PlayerStateType.Block;
         }
