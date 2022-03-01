bd86da2c8a1eacebca90dd0d438b3d52cb5d378f
Lex <totallabyss@gmail.com>
2022-03-01 23:57:59 +0300

    remove log in patrol

---
+++ Assets/Scripts/Tests/PatrolTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PatrolTest : MonoBehaviour
+{
+	public Vector3[] pointsToMove;
+	void Update()
+	{
+		OnDrawGizmosSelected();
+	}
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(pointsToMove[0], 0.2f);
+	}
+}
